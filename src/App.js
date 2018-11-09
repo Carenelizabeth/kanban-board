f162b7fcb5077501198572a918fd32625356acad
Caren Keyes <cejkeyes@gmail.com>
2018-11-09 12:44:42 -0800

    move notes data into app state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Notes from './Notes';
 import './App.css';
 
 import uuidv4 from 'uuid';
 
-const notes = [
-    {
-        id: uuidv4(),
-        task: 'Learn React'
-    },
-    {
-        id: uuidv4(),
-        task: 'Do Laundry'
-    },
-    {
-        id: uuidv4(),
-        task: 'Find a job'
-    }
-]
-
-class App extends Component {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      notes: [
+        {
+            id: uuidv4(),
+            task: 'Learn React'
+        },
+        {
+            id: uuidv4(),
+            task: 'Do Laundry'
+        },
+        {
+            id: uuidv4(),
+            task: 'Find a job'
+        }
+      ]
+    };
+  }
   render() {
+    const {notes} = this.state;
     return (
       <div className="App">
         <Notes notes={notes}/>
