ac267e43fdf1d1c316458beb5b3e23b0c8fe63fb
Caren Keyes <cejkeyes@gmail.com>
2018-11-09 12:38:19 -0800

    move notes data to APP component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Notes from './Notes';
 import './App.css';
+
+import uuidv4 from 'uuid';
+
+const notes = [
+    {
+        id: uuidv4(),
+        task: 'Learn React'
+    },
+    {
+        id: uuidv4(),
+        task: 'Do Laundry'
+    },
+    {
+        id: uuidv4(),
+        task: 'Find a job'
+    }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <Notes notes={notes}/>
+        <button onClick={() => console.log('add note')}>+</button>
       </div>
     );
   }
--- src/Notes.js
+++ src/Notes.js
@@ -1,22 +1,6 @@
 import React from 'react';
-import uuidv4 from 'uuid';
 
-const notes = [
-    {
-        id: uuidv4(),
-        task: 'Learn React'
-    },
-    {
-        id: uuidv4(),
-        task: 'Do Laundry'
-    },
-    {
-        id: uuidv4(),
-        task: 'Find a job'
-    }
-]
-
-export default () => (
+export default ({notes}) => (
     <ul>{notes.map(note => 
         <li key={note.id}>{note.task}</li>)}
     </ul>
